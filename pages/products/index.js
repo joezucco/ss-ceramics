c57a5d4034d7651393da6e44b497ef74ee032278
Joe Zucco <josephzucco@gmail.com>
2021-06-22 00:19:40 -0400

    delete homepage from package json

--- pages/products/index.js
+++ pages/products/index.js
@@ -1,4 +1,3 @@
-import Link from "next/link";
 import { createClient } from "contentful";
 import ProductCard from "../../components/ProductCard";
 
@@ -22,13 +21,6 @@ export default function Products({ products }) {
   console.log(products);
 
   return (
-    // <div class='flex flex-wrap -mx-2 overflow-hidden sm:-mx-1 md:-mx-1 lg:-mx-4 xl:-mx-6'>
-    //   <div class='my-2 px-2 w-full overflow-hidden sm:my-1 sm:px-1 sm:w-1/2 md:my-1 md:px-1 md:w-1/3 lg:my-4 lg:px-4 lg:w-1/3 xl:my-6 xl:px-6 xl:w-1/3'>
-    //     {products.map((product) => (
-    //       <ProductCard key={product.sys.id} product={product} />
-    //     ))}
-    //   </div>
-    // </div>
     <div class='min-h-screen'>
       <div class='flex flex-wrap overflow-hidden '>
         {products.map((product) => (
